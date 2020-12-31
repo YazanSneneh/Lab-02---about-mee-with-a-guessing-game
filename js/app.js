da39de2e5dec7d2550152b29a34e5fc7bed6554d
yazan <yaz.sneneh@gmail.com>
2020-12-31 21:13:57 +0200

    add files

--- js/app.js
+++ js/app.js
@@ -1,79 +1,61 @@
 'use strict';
 var correctAnswers = 0;
-
-// ask user for his/her name
 var userName = prompt('Introduce yourself !');
 alert('Welcome ' + userName + ' let\'s see how much do you know about me ')
 
-// ask user 5 questions about me and convert it to lower case
 
 var nameQuestion = prompt('Is my name Yazan? (Please answer Yes or No)').toLocaleLowerCase();
-if (nameQuestion === 'yes') {     // responde to user my name question answer
-    // console.log('Bingo!');
+if (nameQuestion === 'yes') {
     alert('Bingo!');
     correctAnswers++
 } else if (nameQuestion === 'no') {
-    // console.log('You have no least idea about me');
     alert('You have no least idea about me');
 } else {
-    // console.log('Please answer within Yes or no, refresh page');
     alert('Please answer within Yes or no, refresh page');
 }
 
 var jobQuestion = prompt('Am I a future javascript engineer? (Please answer Yes or No)').toLocaleLowerCase();
-if (jobQuestion === 'yes') {      // responde to user my job question answer
-    // console.log('Good job, extra point!');
+if (jobQuestion === 'yes') {
     alert('Good job, extra point!');
     correctAnswers++
 } else if (jobQuestion === 'no') {
-    // console.log('Nice try!');
     alert('Nice try!');
 } else {
-    // console.log('Please answer within Yes or no, refresh page');
     alert('Please answer within Yes or no, refresh page');
 }
 
 var powerQuestion = prompt('Is my special power I can Fly? (Please answer Yes or No)').toLocaleLowerCase();
-if (powerQuestion === 'yes') {      // responde to user my power question answer
-    console.log('Humans don\'t literally fly!, but they can make things fly');
+if (powerQuestion === 'yes') {
+
     alert('Humans don\'t literally fly!, but they can make things fly');
 } else if (powerQuestion === 'no') {
-    // console.log('Common sense, correct!');
     alert('Common sense, correct!');
     correctAnswers++
 } else {
-    // console.log('Please answer within Yes or no, refresh page');
     alert('Please answer within Yes or no, refresh page');
 }
 
 var favFoodQuestion = prompt('Do I love food so much ? (Please answer Yes or No)').toLocaleLowerCase();
-if (favFoodQuestion === 'yes') {     // responde to user my name food answert
-    // console.log('Good job, extra point!');
+if (favFoodQuestion === 'yes') {
     alert('Good job, extra point!');
     correctAnswers++
 } else if (favFoodQuestion === 'no') {
-    // console.log('Nice try');
     alert('Nice try');
 } else {
-    // console.log('Please answer within Yes or no, refresh page');
     alert('Please answer within Yes or no, refresh page');
 }
 
 var placeQuestion = prompt('Do I live on Mars?(Please answer Yes or No)').toLocaleLowerCase();
-if (placeQuestion === 'yes') {      // responde to user my home place question answer
-    // console.log('Sadly no, you lose a point');
+if (placeQuestion === 'yes') {
     alert('Sadly no, you lose a point');
 } else if (placeQuestion === 'no') {
-    // console.log('Good job, extra point!');
     alert('Good job, extra point!');
     correctAnswers++
 } else {
-    // console.log('Please answer within Yes or no, refresh page');
     alert('Please answer within Yes or no, refresh page');
 }
-// final message
-alert("Nice to meet you " + userName + ' I hope you enjoy your time in our website');
-// Question 6 guess the number
+
+// Question 6
 for (var i = 0; i < 4; i++) {
     var userNumberGuess = Number(prompt('Guess my favourite movie number from 1 to 10 in the list, you have 4 chances'));
     if (userNumberGuess > 5) {
@@ -102,7 +84,8 @@ for (var j = 0; j < 6; j++) {
         }
     }
 }
+alert('Correct answers within 1 - 100: ' + correctAnswersList);
 
-alert('Correct answers are: ' + correctAnswersList);  // replay to user the correct answers list
-
+// final message
+alert("Nice to meet you " + userName + ' I hope you enjoy your time in our website');
 alert('Your correct score is ' + correctAnswers + ' and total questions are 7');
